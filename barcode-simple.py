fd04ad33431ef5f87a19c7eef2e873e02516962d
Jason Best <jbest@brit.org>
2020-10-07 15:57:12 -0500

    cleanup

--- barcode-simple.py
+++ barcode-simple.py
@@ -7,13 +7,8 @@ import re
 import csv
 import os
 import platform
-#import pickle
 #import sqlite3 as lite
 from pyzbar.pyzbar import decode
-#from PIL import Image
-#import cv2
-#import features
-
 
 # File extensions that are scanned and logged
 INPUT_FILE_TYPES = ('.jpg', '.jpeg', '.JPG', '.JPEG')
@@ -63,7 +58,7 @@ def get_actual_filename(name):
         test_name += ["%s[%s]" % (d[:-1], d[-1])]
     res = glob.glob('\\'.join(test_name))
     if not res:
-        #File not found
+        # File not found
         return None
     return res[0]
 
@@ -72,14 +67,14 @@ def casedpath(path):
     r = glob.glob(re.sub(r'([^:/\\])(?=[/\\]|$)', r'[\1]', path))
     return r and r[0] or path
 
-def log_file_data(batch_id=None, batch_path=None, batch_flags=None, closest_model=None,\
+def log_file_data(batch_id=None, batch_path=None, batch_flags=None, \
     image_event_id=None, barcodes=None, image_classifications=None, \
     image_path=None, file_uuid=None, derived_from_file=None):
     basename = os.path.basename(image_path)
     file_name, file_extension = os.path.splitext(basename)
     # Get file creation time
     file_creation_time = datetime.fromtimestamp(creation_date(image_path))
-    #generate MD5 hash
+    # generate MD5 hash
     file_hash = md5hash(image_path)
     datetime_analyzed = datetime.now()
 
@@ -88,15 +83,12 @@ def log_file_data(batch_id=None, batch_path=None, batch_flags=None, closest_mode
         barcodes = str(barcodes)
     else:
         barcodes = ''
-    if closest_model:
-        closest_model = str(closest_model)
-    else:
-        closest_model = ''
 
     reportWriter.writerow([\
     batch_id, batch_path, batch_flags, project_id, \
-    image_event_id, datetime_analyzed, barcodes, image_classifications, closest_model, \
-    image_path, basename, file_name, file_extension, file_creation_time, file_hash, file_uuid, derived_from_file])
+    image_event_id, datetime_analyzed, barcodes, image_classifications, \
+    image_path, basename, file_name, file_extension, file_creation_time, \
+    file_hash, file_uuid, derived_from_file])
 
 # set up argument parser
 ap = argparse.ArgumentParser()
@@ -111,26 +103,25 @@ ap.add_argument("-o", "--output", required=False, \
 args = vars(ap.parse_args())
 
 # set up database
+"""
+# Temporarily removing need for SQLite
 conn = lite.connect('workflow.db')
 cur = conn.cursor()
 try:
     cur.execute('''CREATE TABLE images (id INTEGER PRIMARY KEY, \
         batch_id text, batch_path text, batch_flags text, project_id text, \
         image_event_id text, datetime_analyzed text, \
-        barcodes text, image_classifications text, closest_model text, \
+        barcodes text, image_classifications text, \
         image_path text, basename text, file_name text, file_extension text, \
         file_creation_time text, file_hash text, file_uuid text, derived_from_file text)''')
 except lite.Error as e:
     print(e)
+"""
 
 analysis_start_time = datetime.now()
 batch_id = str(uuid.uuid4())
 batch_path = os.path.realpath(args["source"])
 project_id = args["project"]
-# load models from pickle file
-if args["models"]:
-    print('Loading model histograms.')
-    models = pickle.load(open(args["models"], "rb"))
 
 if args["batch"]:
     batch_flags = args["batch"]
@@ -154,9 +145,9 @@ reportWriter = csv.writer(reportFile, delimiter=FIELD_DELIMITER, escapechar='#')
 # write header
 reportWriter.writerow([\
     "batch_id", "batch_path", "batch_flags", "project_id", \
-    "image_event_id", "datetime_analyzed", "barcodes", "image_classifications", "closest_model",\
-    "image_path", "basename", "file_name", "file_extension", "file_creation_time", "file_hash", "file_uuid", "derived_from_file"])
-    #"ImagePath", "DirPath" , "BaseName", "FileName", "FileExtension", "Code", "CodeType" , "Scan time"])
+    "image_event_id", "datetime_analyzed", "barcodes", "image_classifications", \
+    "image_path", "basename", "file_name", "file_extension", "file_creation_time", \
+    "file_hash", "file_uuid", "derived_from_file"])
 
 #TODO extract information from directory name (imager, station, etc)
 #iterate JPG files in directory passed from args
@@ -194,7 +185,7 @@ for image_path in sorted(glob.glob(os.path.join(directory_path, '*.JPG')), key=o
     else:
         #print('Archive file:', arch_file_path)
         print('Archive file:', os.path.basename(arch_file_path))
-        
+
     # read barcodes from JPG
     barcodes = decode(Image.open(image_path))
     matching_barcodes = []
@@ -210,20 +201,6 @@ for image_path in sorted(glob.glob(os.path.join(directory_path, '*.JPG')), key=o
                 print(symbology_type, data)
     else:
         print('No barcodes found')
-        image_classifications.append('folder')
-        # compare to folder models
-        # save matching models, ranked
-        # generate suggested filename? Or should that be done in rename script?
-        if args["models"] is None:
-            print('No model file provided. No histogram analysis done.')
-        else:
-            image = cv2.imread(image_path)
-            histogram = features.describe(image)
-            best_match = closest_histogram(image_histogram=histogram)
-            [(model_name, model_similarity)] = best_match.items()
-            if 'specimen' in model_name:
-                image_classifications.append('ambiguous')
-            print('best_match', best_match)
 
     scan_end_time = datetime.now()
     # TODO report analysis progress and ETA
@@ -232,7 +209,7 @@ for image_path in sorted(glob.glob(os.path.join(directory_path, '*.JPG')), key=o
     #log CR2 data
     if arch_file_path:
         arch_file_uuid = str(uuid.uuid4())
-        log_file_data(batch_id=batch_id, batch_path=batch_path, batch_flags=batch_flags, closest_model=best_match,\
+        log_file_data(batch_id=batch_id, batch_path=batch_path, batch_flags=batch_flags, \
             image_event_id=image_event_id, barcodes=matching_barcodes, image_classifications=image_classifications_string, \
             image_path=arch_file_path, file_uuid=arch_file_uuid)
     else:
@@ -240,15 +217,16 @@ for image_path in sorted(glob.glob(os.path.join(directory_path, '*.JPG')), key=o
 
     #log JPG data
     derivative_file_uuid = str(uuid.uuid4())
-    log_file_data(batch_id=batch_id, batch_path=batch_path, batch_flags=batch_flags, closest_model=best_match,\
+    log_file_data(batch_id=batch_id, batch_path=batch_path, batch_flags=batch_flags, \
         image_event_id=image_event_id, barcodes=matching_barcodes, image_classifications=image_classifications_string, \
         image_path=image_path, file_uuid=derivative_file_uuid, derived_from_file=arch_file_uuid)
 
 
 analysis_end_time = datetime.now()
-
+"""
 if conn:
     conn.close()
+"""
 print('Started:', analysis_start_time)
 print('Completed:', analysis_end_time)
 print('Files analyized:', files_analyzed)
